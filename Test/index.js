43a13da2bdfbd290a63645828028a1e5ede8052a
CarlosFPD <CarlosFPD@Gmail.com>
2021-12-17 07:48:07 -0500

    Avance js

---
+++ Test/index.js
@@ -0,0 +1,17 @@
+let btn = document.getElementById('mbGuardar');
+btn.addEventListener('click',procesarFormatulario() );
+
+function procesarFormatulario() { 
+ let nombreCapa = document.getElementById('mbNombre');
+ let protocoloCapa = document.getElementById('mbProtocolo');
+ let urlCapa = document.getElementById('mbURL');
+ let minZoomCapa = document.getElementById('mbMinZoom');
+ let maxZoomCapa = document.getElementById('mbMaxZoom');
+ let bandera = 1;
+
+ if (nombreCapa =='') then 
+  {
+    bandera=0;
+  }
+   
+}

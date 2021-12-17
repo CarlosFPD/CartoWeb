31de780cac99c5377813d7d2ba7fec39f460365f
CarlosFPD <CarlosFPD@Gmail.com>
2021-12-17 11:05:52 -0500

    fin del formulario

--- Test/index.js
+++ Test/index.js
@@ -1,17 +1,37 @@
 let btn = document.getElementById('mbGuardar');
-btn.addEventListener('click',procesarFormatulario() );
+let zme = document.getElementById('ZonaMensajeError');
+btn.addEventListener('click',procesarFormatulario );
+
 
 function procesarFormatulario() { 
  let nombreCapa = document.getElementById('mbNombre');
  let protocoloCapa = document.getElementById('mbProtocolo');
  let urlCapa = document.getElementById('mbURL');
- let minZoomCapa = document.getElementById('mbMinZoom');
- let maxZoomCapa = document.getElementById('mbMaxZoom');
+ let minZoomCapa = document.getElementById('mbZoomMin');
+ let maxZoomCapa = document.getElementById('mbZoomMax');
  let bandera = 1;
 
- if (nombreCapa =='') then 
+
+ if (nombreCapa.value =='')
   {
+    alerta('El Nombre de la capa es obligatorio', 'success');
+    bandera=0;
+  }
+ if (protocoloCapa.value  =='')
+  {
+    alerta('El protocolo es obligatorio', 'success');  
+    bandera=0;
+  }
+ if (urlCapa.value  =='')
+  {
+    alerta('La URL es obligatoria', 'success');  
     bandera=0;
   }
    
 }
+
+function alerta(mensaje, tipo) {
+    var wrapper = document.createElement('div')
+    wrapper.innerHTML = '<div class="alert alert-' + tipo + ' alert-dismissible" role="alert">' + mensaje + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
+    zme.append(wrapper)
+  }
